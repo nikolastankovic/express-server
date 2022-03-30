eff929b063ad170a3031e82f73fc0478faf6c757
Nikola Stankovic <nstankovic2107@gmail.com>
2022-03-30 11:01:17 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const cors = require("cors");
+const express = require("express");
+const app = express();
+global.__basedir = __dirname;
+var corsOptions = {
+  origin: "http://localhost:3000"
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+const initRoutes = require("./src/routes");
+app.use(express.urlencoded({ extended: true }));
+initRoutes(app);
+let port = 8080;
+app.listen(port, () => {
+  console.log(`Running at localhost:${port}`);
+});
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controller/file.controller");
+let routes = (app) => {
+  router.post("/upload", controller.upload);
+  router.post("/flow", controller.flow);
+  router.get("/files", controller.getListFiles);
+  router.get("/files/:name", controller.download);
+  app.use(router);
+};
+module.exports = routes;
